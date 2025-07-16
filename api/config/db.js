c4ffa051a715f60a1dfb9a39a886a6ea0bffdcb9
danU <danU>
2025-07-16 17:06:06 -0500

    debuger conexion db 2

--- api/config/db.js
+++ api/config/db.js
@@ -2,12 +2,20 @@ import 'dotenv/config'
 import pg from 'pg'
 const { Pool } = pg
 
+console.log("Conectando con:");
+console.log("  host:", process.env.HOST);
+console.log("  user:", process.env.USER);
+console.log("  database:", process.env.DATABASE);
+console.log("  password:", process.env.PASSWORD);
+console.log("  port:", process.env.PORT);
+
+
 const pool = new Pool({
     host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    port: process.env.PORT,
+    port: Number(process.env.PORT),
     ssl: {
         rejectUnauthorized: false
     }
